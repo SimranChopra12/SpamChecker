ea13555dacbd308fda5504fd447de60b9317dd6e
SimranChopra12 <simi.chopra12@gmail.com>
2025-10-03 15:41:00 -0300

    done until step 3 - training the model

--- SpamChecker.py
+++ SpamChecker.py
@@ -1,5 +1,38 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split # split the dataset into parts to first train the model, and then train it 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import classification_report, confusion_matrix
+
 
 df = pd.read_csv("data/SMSSpamCollection", sep="\t", header=None, names=["label","text"])
-print(df.head())
+#print(df.head())
+
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(df["text"])
+#print(X)
+
+
+# Step 3 : train the model
+
+y = df["label"].map({"ham":0, "spam":1}) 
+# converting to score - ham=0 and spam=1
+
+X_train, X_test, y_train, y_test =  train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+
+clf = LogisticRegression(max_iter=1000)
+clf.fit(X_train, y_train)
+# trains the model
+# iterates 1000 times to make sure it learns
+# .fit is the command for model to learn
+# clf is the classifier - the actual model
+
+y_pred = clf.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+# Accuracy = (Number of correct predictions) / (Total predictions)
+
+
